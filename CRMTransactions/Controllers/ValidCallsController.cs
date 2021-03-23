e83368066044f6b1621e887f787c3bf18d29af4b
Abinaash <abinaash.ks@gmail.com>
2021-03-23 22:54:24 +0530

    Follow up time indian time conversion revoked

--- CRMTransactions/Controllers/ValidCallsController.cs
+++ CRMTransactions/Controllers/ValidCallsController.cs
@@ -60,6 +60,8 @@ namespace CRMTransactions.Controllers
 
             TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
 
+            validCall.EventTime = TimeZoneInfo.ConvertTimeFromUtc(validCall.EventTime.ToUniversalTime(), cstZone);
+
             validCall.UpdatedDateTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
 
             if (id != validCall.ValidCallId)
@@ -101,11 +103,6 @@ namespace CRMTransactions.Controllers
             validCall.EventTime = TimeZoneInfo.ConvertTimeFromUtc(validCall.EventTime.ToUniversalTime(), cstZone);
 
             validCall.UpdatedDateTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);
-
-            if (validCall.FollowUpTime != null)
-            {
-                validCall.FollowUpTime = TimeZoneInfo.ConvertTimeFromUtc(validCall.FollowUpTime.GetValueOrDefault(), cstZone);
-            }
 
             context.ValidCalls.Add(validCall);
 
